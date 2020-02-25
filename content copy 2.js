c73952d3de144bf06473109ff637c8e876615597
dalythu <dalyth@gmail.com>
2020-02-25 17:51:51 -0500

    save fields from HTML

---
+++ content copy 2.js
@@ -0,0 +1,19 @@
+class Task {
+	constructor (email, password){
+		this._email = email;
+		this._password = password;
+	}
+	get email(){
+		return this._email;
+	}
+	get password(){
+		return this._email;
+	}
+	set email(email){
+		this._email = email;
+	}
+	set password(password){
+		this._password = password;
+	}
+
+}
